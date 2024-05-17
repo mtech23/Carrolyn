1fa9a1686000443e8bafc7ab124900cd334b2a99
MUmairqureshi <umairqadeer586@gmail.com>
2024-05-17 00:21:09 -0700

    Revamp Carrolyn

---
+++ src/views/Layout/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Footer } from "../../components/Layout/footer";
+import { Header } from "../../components/Layout/header";
+
+const LayoutTheme = (props) => {
+
+ return (
+    <div className="themeLayout">
+        <div className="headerLayout">
+            <Header />
+        </div>
+        <div className="mainLayout">
+            {props.children}
+        </div>
+        <div className="footerLayouts">
+            <Footer />
+        </div>
+    </div>
+);
+}
+export default LayoutTheme;
